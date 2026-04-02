e900e50bd82f064937334a9203871e9e767c92f8
atoumbre <atoumbre@gmail.com>
2026-04-02 16:25:49 -0400

    test: expand LevitAppLifecycleMixin observer test coverage to verify lifecycle state transitions and cleanup

--- packages/kits/levit_flutter/test/mixins/levit_app_lifecycle_mixin_observer_test.dart
+++ packages/kits/levit_flutter/test/mixins/levit_app_lifecycle_mixin_observer_test.dart
@@ -1,11 +1,58 @@
-import 'package:flutter/material.dart'; import 'package:flutter_test/flutter_test.dart'; import 'package:levit_flutter/levit_flutter.dart';
-class LifecycleController extends LevitController with LevitAppLifecycleMixin {}
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:levit_flutter/levit_flutter.dart';
+
+class ObserverLifecycleController extends LevitController
+    with LevitAppLifecycleMixin {
+  final List<AppLifecycleState> observedStates = <AppLifecycleState>[];
+
+  @override
+  void onAppResumed() => observedStates.add(AppLifecycleState.resumed);
+
+  @override
+  void onAppPaused() => observedStates.add(AppLifecycleState.paused);
+
+  @override
+  void onAppInactive() => observedStates.add(AppLifecycleState.inactive);
+
+  @override
+  void onAppDetached() => observedStates.add(AppLifecycleState.detached);
+
+  @override
+  void onAppHidden() => observedStates.add(AppLifecycleState.hidden);
+}
+
 void main() {
-  testWidgets('LevitAppLifecycleMixin observer state changes', (tester) async {
-    final controller = LifecycleController(); controller.onInit();
-    tester.binding.handleAppLifecycleStateChanged(AppLifecycleState.paused);
-    tester.binding.handleAppLifecycleStateChanged(AppLifecycleState.resumed);
-    try { tester.binding.handleAppLifecycleStateChanged(AppLifecycleState.hidden); } catch (_) {}
-    controller.onClose();
+  group('LevitAppLifecycleMixin observer', () {
+    testWidgets('registers on init and stops receiving events on close', (
+      tester,
+    ) async {
+      final controller = ObserverLifecycleController();
+      final states = <AppLifecycleState>[
+        AppLifecycleState.paused,
+        AppLifecycleState.resumed,
+        AppLifecycleState.inactive,
+        AppLifecycleState.detached,
+        AppLifecycleState.hidden,
+      ];
+
+      controller.onInit();
+
+      for (final state in states) {
+        tester.binding.handleAppLifecycleStateChanged(state);
+        await tester.pump();
+      }
+
+      expect(controller.observedStates, states);
+
+      controller.onClose();
+
+      for (final state in states) {
+        tester.binding.handleAppLifecycleStateChanged(state);
+        await tester.pump();
+      }
+
+      expect(controller.observedStates, states);
+    });
   });
 }
